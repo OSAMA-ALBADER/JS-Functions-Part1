a1c16f257681ee747d370c805e6a92c8f322a4b4
unknown <osama.albader99@gmail.com>
2024-10-07 11:55:05 +0300

    Part 1

---
+++ functions1.js
@@ -0,0 +1,40 @@
+
+function printName (){
+    console.log("my name is osama");
+
+}
+printName()
+
+
+
+function printAge (birthyear){
+   const age = 2024 - birth; 
+    console.log("you age is ");
+    
+}
+
+
+
+function printAgeAndName (Birthyear, yourName){
+    const age = 2024 - birth
+    console.log(`Hello your name is ${yourName} you are ${age} years old`);
+}
+
+
+
+ 
+ 
+function printHello (name, language){
+    if (language == en)console.log(`Hello ${name}`);
+    else if (language == es)console.log (`Hola ${name}`);
+    else if (language == fr)console.log (`Bomjour ${name}`);
+    else if (language == tr)console.log (`Merhaba ${name}`);
+}
+
+
+function printMax (First, Second){
+    
+        if (First > Second)console.log(First + " is the bigger number");
+         else if  (First < Second)console.log(Second + " is the bigger number");
+        
+    }
